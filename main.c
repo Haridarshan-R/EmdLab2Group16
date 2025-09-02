832f9fc075fb83ba973f905be578ff8cadc887f5
Haridarshan R <220020025@iitdh.ac.in>
2025-09-02 23:38:06 +0530

    Lab 2 part 2 main.c

--- main.c
+++ main.c
@@ -6,15 +6,26 @@ int main(void)
 {
     SYSCTL_RCGC2_R |=   0x00000020;
     GPIO_PORTF_LOCK_R = 0x4C4F434B;
-    GPIO_PORTF_CR_R   = 0x17;
-    GPIO_PORTF_DEN_R  = 0x17;
-    GPIO_PORTF_DIR_R  = 0x06;
-    GPIO_PORTF_PUR_R  = 0x11;
+    GPIO_PORTF_CR_R   = 0x0F;
+    GPIO_PORTF_DEN_R  = 0x0F;
+    GPIO_PORTF_DIR_R  = 0x0E;
+    GPIO_PORTF_PUR_R  = 0x01;
+    GPIO_PORTF_DATA_R = 0x02;
+    long int x;
+    int last_state = 0;
+    long int delay = 500000;
     while(1)
     {
-        if ((GPIO_PORTF_DATA_R & 0x11) == 0x11) GPIO_PORTF_DATA_R = 0x00;
-        else if ((GPIO_PORTF_DATA_R & 0x11) == 0x01) GPIO_PORTF_DATA_R = 0x02;
-        else if ((GPIO_PORTF_DATA_R & 0x11) == 0x10) GPIO_PORTF_DATA_R = 0x04;
-        else ((GPIO_PORTF_DATA_R & 0x11) == 0x00) GPIO_PORTF_DATA_R = 0x06;
+        if (((GPIO_PORTF_DATA_R & 0x01) == 0) && (last_state == 0))
+        {
+            last_state = 1;
+            if((GPIO_PORTF_DATA_R & 0x0E) == 0x02) GPIO_PORTF_DATA_R = 0x08;
+            else if((GPIO_PORTF_DATA_R & 0x0E) == 0x08) GPIO_PORTF_DATA_R = 0x04;
+            else GPIO_PORTF_DATA_R = 0x02;
+            x=0;
+            while ( x < delay)
+            x ++ ;
+        }
+        if((GPIO_PORTF_DATA_R & 0x01) != 0) last_state = 0;
     }
 }
