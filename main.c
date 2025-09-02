0ee61fccaca659f0ca911059ade432e2aa8aa3e4
Haridarshan R <220020025@iitdh.ac.in>
2025-09-02 23:41:23 +0530

    Lab 5 part 2 main.c

--- main.c
+++ main.c
@@ -7,26 +7,43 @@
 #define STCURRENT            (*((volatile uint32_t *)0xE000E018))
 
 #define ENABLE   1
-#define CLK_SRC (1<<2)
-#define COUNT   (1<<16)
-#define RED     (1<<1)
+#define CLK_SRC  (1<<2)
+#define COUNT    (1<<16)
+#define RED      (1<<1)
+#define BLUE     (1<<2)
+
+void poll_sw1(void);
+
 int main(void)
 {
-    SYSCTL_RCGC2_R |=   0x00000020;
+    SYSCTL_RCGC2_R |= 0x20;          // Enable clock to Port F
     GPIO_PORTF_LOCK_R = 0x4C4F434B;
-    GPIO_PORTF_CR_R   = 0x0F;
-    GPIO_PORTF_DEN_R  = 0x0F;
-    GPIO_PORTF_DIR_R  = 0x0E;
-    GPIO_PORTF_PUR_R  = 0x11;
+    GPIO_PORTF_CR_R   = 0x1F;        // Unlock PF0–PF4
+    GPIO_PORTF_DEN_R  = 0x1F;        // Digital enable
+    GPIO_PORTF_DIR_R  = 0x0E;        // PF1–PF3 output, PF0/PF4 input
+    GPIO_PORTF_PUR_R  = 0x11;        // Pull-ups for PF0, PF4
     GPIO_PORTF_DATA_R = 0x00;
-    STRELOAD          = (1<<23);
-    STCTRL           |= CLK_SRC;
+
+    STRELOAD  = (1<<24)-1;
+    STCURRENT = 0;                   // Clear current
+    STCTRL   |= CLK_SRC;             // SysTick with core clock
+
     while(1)
     {
-        STCTRL      |= ENABLE;
-        while ((STCTRL & COUNT) == 0);
+        poll_sw1();
+        STCTRL |= ENABLE;
+        while ((STCTRL & COUNT) == 0)
+        {
+            poll_sw1();
+        }
         GPIO_PORTF_DATA_R ^= RED;
-        STCTRL      &= ~ENABLE;
-        STCURRENT    = 0;
+        STCTRL &= ~ENABLE;
+        STCURRENT = 0;
     }
+}
+
+void poll_sw1(void)
+{
+    if ((GPIO_PORTF_DATA_R & 0x01) == 0) GPIO_PORTF_DATA_R |= BLUE;
+    else GPIO_PORTF_DATA_R &= ~BLUE;
 }
